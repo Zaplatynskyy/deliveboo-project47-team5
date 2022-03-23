b0361b14ff56aec141600f48aaa95945d2d6cae2
RobertoVillani <robertovillani58@gmail.com>
2022-03-23 10:09:32 +0100

    add banner

--- resources/js/front/components/pages/Home.vue
+++ resources/js/front/components/pages/Home.vue
@@ -1,92 +1,92 @@
 <template>
-    <div>
-        <div class="home_navbar">
-            <Hero />
-            <Categories :categories="dataShared.categories" />
-        </div>
-
-        <Results />
+  <div>
+    <div class="home_navbar">
+      <Hero />
+      <Categories :categories="dataShared.categories" />
+      <Banner />
     </div>
+
+    <Results />
+  </div>
 </template>
 
 <script>
 import Categories from "../sections/Categories.vue";
 import Hero from "../sections/Hero.vue";
 import Results from "../sections/Results.vue";
+import Banner from "../sections/Banner.vue";
 import dataShared from "../../dataShared";
 
 export default {
-    name: "Home",
-    components: {
-        Hero,
-        Categories,
-        Results,
+  name: "Home",
+  components: {
+    Hero,
+    Categories,
+    Results,
+    Banner,
+  },
+  data() {
+    return {
+      dataShared,
+      categories: [],
+      query: "",
+    };
+  },
+  methods: {
+    getCategoriesAndTags() {
+      axios
+        .get("/api/categories")
+        .then((response) => {
+          dataShared.categories = [...response.data.categories];
+          axios
+            .get("/api/tags")
+            .then((response) => {
+              dataShared.tags = [...response.data.tags];
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     },
-    data() {
-        return {
-            dataShared,
-            categories: [],
-            query: "",
-        };
+    saveQuery(value) {
+      this.query = value;
     },
-    methods: {
-        getCategoriesAndTags() {
-            axios
-                .get("/api/categories")
-                .then((response) => {
-                    dataShared.categories = [...response.data.categories];
-                    axios
-                        .get("/api/tags")
-                        .then((response) => {
-                            dataShared.tags = [...response.data.tags];
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        },
-        saveQuery(value) {
-            this.query = value;
-        },
+  },
+  created() {
+    this.getCategoriesAndTags();
+  },
+  watch: {
+    "dataShared.restaurants"(newValue) {
+      if (dataShared.restaurants.length > 0 || dataShared.noResultsFound) {
+        setTimeout(() => {
+          // const restaurant = document.querySelectorAll(".restaurant")
+          // console.log(restaurant[restaurant.length - 1]);
+          // restaurant[restaurant.length - 1].scrollIntoView({
+          //     behavior: "smooth",
+          // });
+          window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth",
+          });
+        }, 10);
+      }
     },
-    created() {
-        this.getCategoriesAndTags();
-    },
-    watch: {
-        "dataShared.restaurants"(newValue) {
-            if (
-                dataShared.restaurants.length > 0 ||
-                dataShared.noResultsFound
-            ) {
-                setTimeout(() => {
-                    // const restaurant = document.querySelectorAll(".restaurant")
-                    // console.log(restaurant[restaurant.length - 1]);
-                    // restaurant[restaurant.length - 1].scrollIntoView({
-                    //     behavior: "smooth",
-                    // });
-                    window.scrollTo({
-                        top: document.body.scrollHeight,
-                        behavior: "smooth",
-                    });
-                }, 10);
-            }
-        },
-    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .categories {
-    width: 200px;
-    text-decoration: underline;
-    cursor: pointer;
+  width: 200px;
+  text-decoration: underline;
+  cursor: pointer;
 
-    &:hover {
-        text-decoration: none;
-        color: #3490dc;
-    }
+  &:hover {
+    text-decoration: none;
+    color: #3490dc;
+  }
 }
 </style>
