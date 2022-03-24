dec7f04f8f0dd63c96e81e4e6fa61a7091355338
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-03-24 15:31:46 +0100

    responsive orders & header style

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,70 +3,116 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-12 col-lg-6 list_order">
+                <div class="card">
+                    <h3 class="card-header">Ordini in arrivo</h3>
+                    <div class="card_orders">
+                        @if (count($orders) > 0)
+                            @foreach ($orders as $order)
+                                <div class="card-body my-4">
+                                    <h4>Ordine #{{ $order->id }}</h4>
+
+                                    <div class="row_info">
+                                        <div class="label">Cliente</div>
+                                        <div class="my_txt name_order">
+                                            <span>{{ $order->name }}</span>
+                                            <span>{{ $order->cognome }}</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="row_info">
+                                        <div class="label">Indirizzo</div>
+                                        <div class="my_txt address">{{ $order->address }}</div>
+                                    </div>
+
+                                    <div class="row_info">
+                                        <div class="label d-flex justify-content-between">
+                                            <span>Telefono</span>
+                                            <span>Importo</span>
+                                        </div>
+                                        <div class="my_txt telephone_price d-flex justify-content-between">
+                                            <span>{{ $order->telephone }}</span>
+                                            <span>{{ $order->total }}€</span>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="buttons d-flex justify-content-between mt-3">
+                                        <button type="button" class="btn btn-info mt-1"><a class="text-white"
+                                                href="{{ route('orders.show', $order->id) }}">Visualizza</a></button>
+                                        <form action="{{ route('orders.approves', $order->id) }}" method="POST">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit"
+                                                class="my_btn btn btn-success mt-1 text-white">Approva</button>
+                                        </form>
+                                        <form action="{{ route('orders.disapproves', $order->id) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit" class="my_btn btn btn-danger mt-1">Annulla</button>
+                                        </form>
+                                    </div>
+                                
+                                    {{-- <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Nome</th>
+                                                <th scope="col">Cognome</th>
+                                                <th scope="col">Indirizzo</th>
+                                                <th scope="col">Telefono</th>
+                                                <th scope="col">Totale</th>
+                                                <th scope="col">Azioni</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            @foreach ($orders as $order)
+                                                <tr>
+                                                    <td>{{ $order->name }}</td>
+                                                    <td>{{ $order->cognome }}</td>
+                                                    <td>{{ $order->address }}</td>
+                                                    <td>{{ $order->telephone }}</td>
+                                                    <td>{{ $order->total }}€</td>
+                                                    <td>
+                                                        <button type="button" class="btn btn-info mt-1"><a class="text-white"
+                                                                href="{{ route('orders.show', $order->id) }}">Visualizza</a></button>
+                                                        <form action="{{ route('orders.approves', $order->id) }}" method="POST">
+                                                            @csrf
+                                                            @method('PATCH')
+                                                            <button type="submit"
+                                                                class="my_btn btn btn-success mt-1 text-white">Approva</button>
+                                                        </form>
+                                                        <form action="{{ route('orders.disapproves', $order->id) }}"
+                                                            method="POST">
+                                                            @csrf
+                                                            @method('PATCH')
+                                                            <button type="submit" class="my_btn btn btn-danger mt-1">Annulla</button>
+                                                        </form>
+
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+
+                                        </tbody>
+                                    </table> --}}
+                                </div>
+                            @endforeach
+                        @else
+                            <div class="card-body">
+                                <p>Non ci sono nuovi ordini</p>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
                 <div class="card">
                     <div class="card-header">
                         Statistiche
                     </div>
-
+    
                     <div class="card-body">
                         <canvas id="popChart" width="600" height="400"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Ordini in arrivo</div>
-
-                    <div class="card-body">
-                        @if (count($orders) > 0)
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Nome</th>
-                                        <th scope="col">Cognome</th>
-                                        <th scope="col">Indirizzo</th>
-                                        <th scope="col">Telefono</th>
-                                        <th scope="col">Totale</th>
-                                        <th scope="col">Azioni</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-                                    @foreach ($orders as $order)
-                                        <tr>
-                                            <td>{{ $order->name }}</td>
-                                            <td>{{ $order->cognome }}</td>
-                                            <td>{{ $order->address }}</td>
-                                            <td>{{ $order->telephone }}</td>
-                                            <td>{{ $order->total }}€</td>
-                                            <td>
-                                                <button type="button" class="btn btn-info mt-1"><a class="text-white"
-                                                        href="{{ route('orders.show', $order->id) }}">Visualizza</a></button>
-                                                <form action="{{ route('orders.approves', $order->id) }}" method="POST">
-                                                    @csrf
-                                                    @method('PATCH')
-                                                    <button type="submit"
-                                                        class="my_btn btn btn-success mt-1 text-white">Approva</button>
-                                                </form>
-                                                <form action="{{ route('orders.disapproves', $order->id) }}"
-                                                    method="POST">
-                                                    @csrf
-                                                    @method('PATCH')
-                                                    <button type="submit" class="my_btn btn btn-danger mt-1">Annulla</button>
-                                                </form>
-
-                                            </td>
-                                        </tr>
-                                    @endforeach
-
-                                </tbody>
-                            </table>
-                        @else
-                            <p>Non ci sono nuovi ordini</p>
-                        @endif
                     </div>
                 </div>
             </div>
